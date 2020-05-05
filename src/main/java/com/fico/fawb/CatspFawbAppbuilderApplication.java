718ac16e9b89f5b119a9c0971099ea716f874fb2
Rabindra Patra <RabindraPatra@corp.fairisaac.com>
2020-05-05 22:31:59 +0530

    initial commit

---
+++ src/main/java/com/fico/fawb/CatspFawbAppbuilderApplication.java
@@ -0,0 +1,13 @@
+package com.fico.fawb;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class CatspFawbAppbuilderApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(CatspFawbAppbuilderApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/fico/fawb/CatspFawbAppbuilderApplicationTests.java
@@ -0,0 +1,13 @@
+package com.fico.fawb;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class CatspFawbAppbuilderApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
