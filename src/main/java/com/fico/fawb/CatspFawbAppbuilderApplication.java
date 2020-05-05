1bb4fc4d0ed30dca74c2604181ef729f5ba739ef
Rabindra Patra <RabindraPatra@corp.fairisaac.com>
2020-05-05 23:42:46 +0530

    use env variable for debug

--- src/main/java/com/fico/fawb/CatspFawbAppbuilderApplication.java
+++ src/main/java/com/fico/fawb/CatspFawbAppbuilderApplication.java
@@ -2,13 +2,17 @@ package com.fico.fawb;
 
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.CommandLineRunner;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.core.env.Environment;
 
 @SpringBootApplication
 public class CatspFawbAppbuilderApplication implements CommandLineRunner {
 
+	@Autowired
+	private Environment environment;
 	private static final Logger LOGGER = LoggerFactory.getLogger(CatspFawbAppbuilderApplication.class);
 	public static void main(String[] args) {
 		SpringApplication.run(CatspFawbAppbuilderApplication.class, args);
@@ -17,5 +21,16 @@ public class CatspFawbAppbuilderApplication implements CommandLineRunner {
 	@Override
 	public void run(String... args) throws Exception {
 		LOGGER.info("java.version: "+System.getProperty("java.version"));
+		LOGGER.info("run.debug: "+environment.getProperty("run.debug"));
+		LOGGER.info("clone & build project");
+
+		if(environment.getProperty("run.debug")==null)
+		{
+			LOGGER.info("terminating");
+			System.exit(0);
+		}
+		else{
+			LOGGER.info("running in debug mode");
+		}
 	}
 }
